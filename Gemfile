e0a6813c1df5a6d674af9afe166a01b07d3086d8
firestorm451 <mzwick1684@hotmail.com>
2016-04-14 16:20:59 -0400

    Basic user logic put in

--- Gemfile
+++ Gemfile
@@ -9,9 +9,7 @@ gem 'pg', '~> 0.15'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-# Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.1.0'
-# See https://github.com/rails/execjs#readme for more supported runtimes
+
 # gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
@@ -31,7 +29,7 @@ gem 'simple_form'
 gem 'giphy'
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  private
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+
+  def require_user
+    unless current_user
+      flash[:danger] = "You need to login first."
+      redirect_to :root
+    end
+  end
+
+  def disallow_user
+    if current_user
+      flash[:warning] = "You're already logged in."
+      redirect_to :root
+    end
+  end
 end
