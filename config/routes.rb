bf4e68c6e6ca63f804d7af585923cc0ec2d00d90
J-Henry <JH@JH.local>
2016-04-15 11:45:32 -0400

    Edit routes, fewer resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,16 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :users, except: [:index] do
-    resources :gifs, except: [:index, :edit, :update]
-  end
-
-  resources :gifs, only: [:index]
-
-  # You can have the root of your site routed with "root"
-  root 'gifs#index'
-
+  root   'gifs#index'
   get    '/signup' => 'users#new'
   get    '/login'  => 'sessions#new', as: :login
   post   '/login'  => 'sessions#create', as: :login_create
   delete '/logout' => 'sessions#destroy'
+
+  resources :users, except: [:index] do
+    resources :gifs, except: [:index, :edit, :update]
+  end
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
