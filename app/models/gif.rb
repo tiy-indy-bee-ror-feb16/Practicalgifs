e50b47e2a2be04ad4481c6fb3a4bf73a4b0b7a7a
J-Henry <JH@JH.local>
2016-04-16 13:47:44 -0400

    Valitate Gif url & filetype

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,3 +1,8 @@
 class Gif < ActiveRecord::Base
   belongs_to :user
+
+  validates :url, presence: true,
+                  format: { with: URI.regexp, message: 'must be a valid URL like http://media.giphy.com/media/tlpVdqCrewXFC/giphy.gif'}, if: Proc.new { |a| a.url.present? }
+  validates :url, format: { with: %r{.(gif)\Z}i, message: 'must be a GIF like http://media.giphy.com/media/tlpVdqCrewXFC/giphy.gif.' }
+
 end
