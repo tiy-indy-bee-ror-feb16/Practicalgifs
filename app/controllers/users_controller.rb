56de38237b1215c82c7fd53c5cc2deab98a651ad
firestorm451 <mzwick1684@hotmail.com>
2016-04-17 15:44:18 -0400

    Sigh still stuck

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :require_user, only: [:new]
+  before_action :disallow_user, only: [:new, :create]
 
   def index
     @users = User.all
